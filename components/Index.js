cc7fe5e34824a7ccbaca738b8c54d044335ab4b8
Diogo <diogoscohati@outlook.com>
2022-08-14 12:45:47 -0300

    First Commit

---
+++ components/Index.js
@@ -0,0 +1,30 @@
+import HomeHeader from "./HomeHeader"
+import FocusedStatusBar from "./FocusedStatusBar"
+import NFTCard from "./NFTCard"
+import { CircleButton, RectButton } from "./Button"
+import DetailsBid from "./DetailsBid"
+import DetailsDesc from "./DetailsDesc"
+import {
+    SubInfo,
+    NFTTitle,
+    EthPrice,
+    ImageCmp,
+    People,
+    EndDate
+} from "./SubInfo"
+
+export {
+    HomeHeader,
+    NFTCard,
+    FocusedStatusBar,
+    CircleButton,
+    RectButton,
+    SubInfo,
+    NFTTitle,
+    EthPrice,
+    ImageCmp,
+    People,
+    EndDate,
+    DetailsBid,
+    DetailsDesc
+}
